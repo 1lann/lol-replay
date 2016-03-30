55cd8a8977a7c46a95b532f7822f9d1e5895c7fa
Jason Chu <1lann@users.noreply.github.com>
2016-03-30 11:30:37 +0800

    Add responsive viewport meta tag

--- server/view.go
+++ server/view.go
@@ -324,6 +324,7 @@ var pageSource = `<!DOCTYPE html>
 <html>
 <head>
 	<title>LoL Replay</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="/bulma.min.css">
 	<link rel="stylesheet" type="text/css" href="/style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
